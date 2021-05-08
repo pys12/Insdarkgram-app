1c34fa9812f3c0540457f857bce10e9f1babd8ef
pys12 <peiyishi11@gmail.com>
2021-05-08 13:03:02 -0400

    setup .env

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const Insta = require('./models/insta.js');
 const db = mongoose.connection;
+const URI = process.env.MONGODB_URI;
 
+require('dotenv').config();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(methodOverride('_method'));
 //route
 app.use('/profile',require('./routes/profile.js'))
 //css
 app.use(express.static(__dirname + '/public'));
 
-mongoose.connect('mongodb://localhost:27017/insta', {
+mongoose.connect(URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
@@ -26,6 +27,7 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log('connected to mongo')
 });
+
 app.listen(PORT, () => {
     console.log('listening on port', PORT)
 })
